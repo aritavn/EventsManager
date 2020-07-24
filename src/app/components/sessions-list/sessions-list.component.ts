9980e624efc2f3ddf48e38da7f5de0ec3f042383
aritavn <anaritavn@gmail.com>
2020-07-24 19:28:14 +0100

    Filter sessions list by level

--- src/app/components/sessions-list/sessions-list.component.ts
+++ src/app/components/sessions-list/sessions-list.component.ts
@@ -1,11 +1,27 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 import { Session } from '../../models/session.model'
 
 @Component({
     selector: 'sessions-list',
     templateUrl: './sessions-list.component.html'
 })
-export class SessionsListComponent {
+export class SessionsListComponent implements OnChanges {
 
     @Input() sessions: Session[]
+    @Input() filterBy: string
+    visibleSessions: Session[] = []
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy)
+        }
+    }
+
+    filterSessions(filter: string): void {
+        if (filter === 'all') {
+            this.visibleSessions = this.sessions.slice(0)
+        } else {
+            this.visibleSessions = this.sessions.filter(session => session.level.toLocaleLowerCase() === filter)
+        }
+    }
 }
