d293ff7bd83f838927f5aa02e7092a3231768f4a
aritavn <anaritavn@gmail.com>
2020-09-03 21:47:42 +0100

    Persist authentication across page refreshes

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { AuthenticationService } from './services/authentication.service'
 
 @Component({
     selector: 'events-app',
     templateUrl: './events-app.component.html'
 })
-export class EventsAppComponent {
-    title = 'ng-fundamentals'
+export class EventsAppComponent implements OnInit {
+    
+    constructor(private authService: AuthenticationService) {}
+
+    ngOnInit() {
+        this.authService.checkAuthenticationStatus().subscribe()
+    }
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -33,6 +33,15 @@ export class AuthenticationService {
         return !!this.currentUser
     }
 
+    checkAuthenticationStatus() {
+        return this.http.get('/api/currentIdentity')
+            .pipe(tap(data => {
+                if (data instanceof Object) {
+                    this.currentUser = <User> data
+                }
+            }))
+    }
+
     updateCurrentUser(firstName: string, lastName: string) {
         this.currentUser.firstName = firstName
         this.currentUser.lastName = lastName
