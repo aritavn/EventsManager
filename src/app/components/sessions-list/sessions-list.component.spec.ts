70a20adbb8f41c43d4f75817b3fa75dedfe71f21
aritavn <anaritavn@gmail.com>
2020-09-09 16:44:25 +0100

    Add unit tests

---
+++ src/app/components/sessions-list/sessions-list.component.spec.ts
@@ -0,0 +1,44 @@
+import { SessionsListComponent } from './sessions-list.component'
+import { Session } from '../../models/session.model'
+
+describe('SessionsListComponent', () => {
+    let component: SessionsListComponent
+    let mockAuthService
+    let mockVoterService
+
+    beforeEach(() => {
+        component = new SessionsListComponent(mockAuthService, mockVoterService)
+    })
+
+    describe('ngOnChanges', () => {
+        it('should filter the sessions correctly', () => {
+            component.sessions = <Session[]> [
+                {name: 'session 1', level: 'intermediate'},
+                {name: 'session 2', level: 'intermediate'},
+                {name: 'session 3', level: 'beginner'}
+            ]
+            component.filterBy = 'intermediate'
+            component.sortBy = 'name'
+            component.eventId = 3
+
+            component.ngOnChanges()
+
+            expect(component.visibleSessions.length).toBe(2)
+        })
+
+        it('should sort the sessions correctly', () => {
+            component.sessions = <Session[]> [
+                {name: 'session 1', level: 'intermediate'},
+                {name: 'session 3', level: 'intermediate'},
+                {name: 'session 2', level: 'beginner'}
+            ]
+            component.filterBy = 'all'
+            component.sortBy = 'name'
+            component.eventId = 3
+
+            component.ngOnChanges()
+
+            expect(component.visibleSessions[2].name).toBe('session 3')
+        })
+    })
+})
